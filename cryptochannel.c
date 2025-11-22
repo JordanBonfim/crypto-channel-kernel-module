81012aec2009511cdf0cf5d76d7f993704e8d42d
JordanBonfim <jordanduzabonfim@gmail.com>
2025-11-22 16:32:45 -0300

    character device creation

--- cryptochannel.c
+++ cryptochannel.c
@@ -6,6 +6,14 @@
 #include <linux/printk.h> // Needed for pr_info()
 #include <linux/device.h> // Para criar a classe e o dispositivo no sistema (o que faz o arquivo aparecer no /dev).
 #include <linux/uaccess.h> //Para futuramente mover dados entre o usuário e o kernel (segurança).
+#include <linux/slab.h>  // Para kmalloc
+#include <linux/mutex.h> // Para mutex
+
+#define BUFFER_SIZE 1024
+static char *kernel_buffer; // Buffer no kernel
+static DEFINE_MUTEX(crypto_mutex); // Mutex para proteger o acesso ao buffer
+static int data_length = 0; // Tamanho dos dados no buffer
+
 static ssize_t device_read(struct file *file, char __user *buf, size_t count, loff_t *offset){
     // Implementar leitura do dispositivo
     pr_info("CRYPTOCHANNEL: READ OPERATION\n");
@@ -37,21 +45,48 @@ struct file_operations fops = {
     .release = device_release
 };
 
-  
+// Variável GLOBAL para guardar o ID do driver
+static int major_number;
+
 int init_module(void)  
 {  
     pr_info("CRYPTOCHANNEL: STARTED\n");  
 
- 
+    major_number = register_chrdev(0, "cryptochannel", &fops);
+    if (major_number < 0) {
+        pr_info("CRYPTOCHANNEL: FAILED TO REGISTER DEVICE\n");
+        return major_number;
+    }
+    pr_info("CRYPTOCHANNEL: REGISTERED WITH MAJOR NUMBER %d\n", major_number);
     /* A nonzero return means init_module failed; module can't be loaded. */
  
+
+    kernel_buffer = (BUFFER_SIZE, GFP_KERNEL); 
+
+    // Verifica se a alocação funcionou 
+    if (!kernel_buffer) {
+        pr_info("CRYPTOCHANNEL: FAILED TO ALLOCATE MEMORY\n");
+        
+        // Se falhou memória, desfazer o registro do dispositivo antes de sair
+        unregister_chrdev(major_number, "cryptochannel");
+        
+        return -ENOMEM; // Código de erro padrão para "Out of Memory"
+    }
+
+    // Limpar a memória com zeros
+    memset(kernel_buffer, 0, BUFFER_SIZE);
+
     return 0;  
 }  
   
 void cleanup_module(void)  
 {  
-    pr_info("CRYPTOCHANNEL: ENDED\n");  
-
+    // Liberar a memória alocada
+    if(kernel_buffer){
+        kfree(kernel_buffer);
+    }
+    pr_info("CRYPTOCHANNEL: ENDED\n");
+    unregister_chrdev(major_number, "cryptochannel");
 }
  
  
